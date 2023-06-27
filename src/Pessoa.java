89db2b02ec1af1fee8c52ed11a98b575f3bcea7e
Daphiny Ramos <daphinyramos14@gmail.com>
2023-06-27 09:10:26 -0300

    cadastro pessoa

---
+++ src/Pessoa.java
@@ -0,0 +1,18 @@
+public class Pessoa {
+
+    String nome;
+    String CPF;
+    String RG;
+    String telefone;
+    String telefoneFamilia;
+    String endereco;
+    boolean assinarTermo;
+
+    public Pessoa (String nome, String CPF, String RG,String telefone,String telefoneFamilia,String endereco)
+        this.nome = "Marcelo";
+        this.CPF = "59482631759";
+        this.RG = "684230175";
+        this.telefone = "11945023687";
+        this.telefoneFamilia = "11963054786";
+        this.endereco = "rua piracema - 47 - Barueri/SP";
+}
